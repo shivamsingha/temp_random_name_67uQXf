204ed1d6739f924abc5b61fad7501edc1e190705
shivam <shivamgit@pm.me>
2020-04-26 15:10:44 +0530

    Add react-native and gatsbyjs
    Signed-off-by: shivam <shivamgit@pm.me>

---
+++ common/gulpfile.js
@@ -0,0 +1,16 @@
+const { src, dest, watch } = require('gulp');
+const ts = require('gulp-typescript');
+
+const tsProject = ts.createProject('tsconfig.json');
+
+const build = () => tsProject.src()
+  .pipe(tsProject())
+  .pipe(src(['src/**', '!src/**/*.tsx', '!src/**/*.ts', '!src/**/*.js']))
+  .pipe(dest('dist'));
+
+function start() {
+  watch('src/**', {events:'all'}, build)
+}
+
+module.exports={start};
+exports.default = build;
