526ab1c9a45bdadd0884040e978fc83c536e3937
shivam <shivamgit@pm.me>
2020-04-28 12:36:10 +0530

    Add eslint sort imports and vars rule
    Signed-off-by: shivam <shivamgit@pm.me>

--- common/.eslintrc.js
+++ common/.eslintrc.js
@@ -1,36 +1,41 @@
 module.exports = {
-    "env": {
-        "browser": true,
-        "es6": true
+  env: {
+    browser: true,
+    es6: true
+  },
+  extends: [
+    'eslint:recommended',
+    'plugin:react/recommended',
+    '@react-native-community',
+    'plugin:@typescript-eslint/eslint-recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:prettier/recommended',
+    'prettier/react'
+  ],
+  globals: {
+    Atomics: 'readonly',
+    SharedArrayBuffer: 'readonly'
+  },
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true
     },
-    "extends": [
-        "eslint:recommended",
-        "plugin:react/recommended",
-        '@react-native-community',
-        "plugin:@typescript-eslint/eslint-recommended",
-        "plugin:@typescript-eslint/recommended",
-        "plugin:prettier/recommended",
-        "prettier/react"
-    ],
-    "globals": {
-        "Atomics": "readonly",
-        "SharedArrayBuffer": "readonly"
-    },
-    "parser": "@typescript-eslint/parser",
-    "parserOptions": {
-        "ecmaFeatures": {
-            "jsx": true
-        },
-        "ecmaVersion": 2018,
-        "sourceType": "module"
-    },
-    "plugins": [
-        "react",
-        "react-hooks",
-        "react-native",
-        "@typescript-eslint"
-    ],
-    "ignorePatterns": ["dist/", "node_modules/", "gulpfile.js"],
-    "rules": {
-    }
+    ecmaVersion: 2018,
+    sourceType: 'module'
+  },
+  plugins: ['react', 'react-hooks', 'react-native', '@typescript-eslint'],
+  ignorePatterns: ['dist/', 'node_modules/', 'gulpfile.js'],
+  rules: {
+    'sort-vars': ['error'],
+    'sort-imports': [
+      'warn',
+      {
+        ignoreCase: false,
+        ignoreDeclarationSort: true,
+        ignoreMemberSort: false,
+        memberSyntaxSortOrder: ['none', 'all', 'multiple', 'single']
+      }
+    ]
+  }
 };
